00dbd5ca7fa30f6f82047eedec4a06bc21b6a0db
jiofenuipoa <r.y.a.pgee@gmail.com>
2026-01-22 13:05:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,9 @@
 
 import streamlit as st
 
-# Basic layout components
 st.title("Welcome to my App")
 st.write("This is a simple Streamlit application.")
 
-# Adding a simple interactive widget
 name = st.text_input("Enter your name", "Guest")
 st.write(f"Hello, {name}!")
 
